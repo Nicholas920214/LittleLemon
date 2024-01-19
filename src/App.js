aac72ab5bdb274b4b3e1b15329c57eac14ea0fcd
Nicholas <nico20030214@gmail,com>
2024-01-19 15:41:21 +0800

    new created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {HashRouter, Routes, Route} from "react-router-dom";
+import pages from './utils/pages';
+import Layout from "./components/layout/Layout";
+import Home from "./components/pages/Home";
+import About from './components/pages/About';
+import UnderConstruction from "./components/pages/UnderConstruction";
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import NotFound from './components/pages/NotFound';
+import ScrollToTop from "./ScrollToTop"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        {/* Scroll To Top wrap all the things */}
+        <ScrollToTop />
+        <Routes>
+          <Route 
+            path={pages.get("home").path}
+            element={
+              <React.Suspense>
+                <Layout />
+              </React.Suspense>
+            }
+          >
+            <Route 
+              path={pages.get("home").path}
+              element={
+                <React.Suspense>
+                  <Home />
+                </React.Suspense>
+              }
+            />
+            <Route 
+              path={pages.get("about").path}
+              element={
+                <React.Suspense>
+                  <About />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={pages.get("bookings").path}
+              element={
+                <React.Suspense>
+                  <Bookings />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path={pages.get("confirmedBooking").path}
+              element={
+                <React.Suspense>
+                  <ConfirmedBooking />
+                </React.Suspense>
+              } 
+            />
+            <Route
+              path={pages.get("orderOnline").path}
+              element={
+                <React.Suspense>
+                  <UnderConstruction />
+                </React.Suspense>
+              }
+            />
+          </Route>
+          <Route
+            path="*"
+            element={
+              <React.Suspense>
+                <NotFound />
+              </React.Suspense>
+            } 
+          />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
